55f2964f77ce56a22876f59cc58a2b8bf2bd9023
jacksmithinsulander <666.jack.smith@protonmail.com>
2025-04-22 09:55:07 +0200

    fix

--- src/contracts/mod.rs
+++ src/contracts/mod.rs
@@ -1,4 +1,3 @@
-#[cfg(any(feature = "sh-usd"))]
+
 pub mod sh_usd;
-#[cfg(any(feature = "sh-usd", feature = "manager"))]
 pub mod manager;
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 #![cfg_attr(target_arch = "wasm32", no_main, no_std)]
 
-#[cfg(target_arch = "wasm32")]
-pub use microstable::user_entrypoint;
+#[cfg( target_arch = "wasm32")]
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    core::arch::wasm32::unreachable()
+}
 
 #[cfg(not(target_arch = "wasm32"))]
 #[doc(hidden)]
